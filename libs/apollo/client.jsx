261f91bca05f0a67e79ca429df1081322e943c5f
Nesar Ahmed <nesar@fcub.edu.bd>
2024-06-23 01:13:58 +0600

    :wrench:  Apollo Client route updates 3

--- libs/apollo/client.jsx
+++ libs/apollo/client.jsx
@@ -10,7 +10,7 @@ export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
     cache: new InMemoryCache(),
     link: new HttpLink({
       // this needs to be an absolute url, as relative urls cannot be used in SSR
-      uri: process.env.WP_URL + "/gql",
+      uri: process.env.NEXT_PUBLIC_BASE_URL + "/gql",
       headers: {
         "Content-Type": "application/json",
         credentials: "include",
